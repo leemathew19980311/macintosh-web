cd7585942289a8703446ab7924a0012a9874e20c
leemathew19980311 <jkwei@foxmail.com>
2024-11-02 16:03:52 +0800

    feat: fix dock

--- app/components/mainContent/dock/config.tsx
+++ app/components/mainContent/dock/config.tsx
@@ -1,48 +1,79 @@
-const defaultConfig = [
+enum DockItemType {
+  APP,
+  SEPARATOR,
+}
+type DockItem =
+  | { type: DockItemType.SEPARATOR }
+  | {
+      label: string;
+      iconPath: string;
+      isActive: boolean;
+      type: DockItemType.APP;
+    };
+
+const defaultConfig: DockItem[] = [
   {
     label: "Finder",
     iconPath: "/icons/finder.svg",
     isActive: true,
+    type: DockItemType.APP,
   },
   {
     label: "Launch Pad",
     iconPath: "/icons/launchPad.svg",
     isActive: false,
+    type: DockItemType.APP,
   },
   {
     label: "Mails",
     iconPath: "/icons/mails.svg",
     isActive: false,
+    type: DockItemType.APP,
   },
   {
     label: "Contacts",
     iconPath: "/icons/contacts.svg",
     isActive: false,
+    type: DockItemType.APP,
   },
   {
     label: "Calendar",
     iconPath: "/icons/calendar.svg",
     isActive: false,
+    type: DockItemType.APP,
   },
   {
     label: "Maps",
     iconPath: "/icons/maps.svg",
     isActive: false,
+    type: DockItemType.APP,
   },
   {
     label: "Messages",
     iconPath: "/icons/messages.svg",
     isActive: false,
+    type: DockItemType.APP,
   },
   {
     label: "FaceTime",
     iconPath: "/icons/faceTime.svg",
     isActive: false,
+    type: DockItemType.APP,
   },
   {
     label: "App Store",
     iconPath: "/icons/appleStore.svg",
     isActive: false,
+    type: DockItemType.APP,
+  },
+  {
+    type: DockItemType.SEPARATOR,
+  },
+  {
+    label: "Trash",
+    iconPath: "/icons/trashFull.svg",
+    isActive: false,
+    type: DockItemType.APP,
   },
 ];
 const ActiveIcon = ({ fill = "#808080" }: { fill: string }) => (
@@ -60,4 +91,4 @@ const ActiveIcon = ({ fill = "#808080" }: { fill: string }) => (
   </svg>
 );
 
-export { defaultConfig, ActiveIcon };
+export { defaultConfig, ActiveIcon, DockItemType };
--- app/components/mainContent/dock/index.tsx
+++ app/components/mainContent/dock/index.tsx
@@ -1,22 +1,58 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { ActiveIcon, defaultConfig } from "./config";
+import { ActiveIcon, defaultConfig, DockItemType } from "./config";
+import clsx from "clsx";
+import { useState } from "react";
 
 const Dock = () => {
-  console.log("Dock");
+  const [list, setList] = useState(defaultConfig);
+  const mouseMoveHandler = (e: React.MouseEvent<HTMLDivElement>) => {
+    const buttons = document.querySelectorAll("button[data-index]");
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+
+    const distances = Array.from(buttons).map((button) => {
+      const rect = button.getBoundingClientRect();
+      const buttonCenterX = rect.left + rect.width / 2;
+      const buttonCenterY = rect.top + rect.height / 2;
+      const distance = Math.sqrt(
+        Math.pow(buttonCenterX - mouseX, 2) +
+          Math.pow(buttonCenterY - mouseY, 2)
+      );
+      return { button, distance };
+    });
+
+    distances.sort((a, b) => a.distance - b.distance);
+
+    const closestButtons = distances.slice(0, 2).map((item) => item.button);
+    console.log("Closest buttons:", closestButtons);
+  };
   return (
     <div className="absolute bottom-2 h-[65px] left-0 right-0 flex justify-center items-center">
-      <div className="bg-[#F6F6F6]/[0.36] rounded-2xl border-[#1A1A1A] border-solid border-opacity-10 border h-full flex gap-x-2 items-center px-2">
-        {defaultConfig.map((item) => {
-          return (
-            <Button
-              variant="link"
-              className="flex p-0 h-auto flex-col items-center gap-0  transition-all duration-300 ease-in-out"
-            >
-              <img src={item.iconPath} width={55} />
-              {item.isActive && <ActiveIcon fill="#808080" />}
-            </Button>
-          );
+      <div
+        onMouseMove={mouseMoveHandler}
+        className="bg-[#F6F6F6]/[0.36] rounded-2xl border-[#1A1A1A] border-solid border-opacity-10 border h-full flex items-center px-1 pt-1 transition-all duration-300 ease-in-out"
+      >
+        {list.map((item, index) => {
+          if (item.type === DockItemType.APP) {
+            return (
+              <Button
+                data-index={index}
+                variant="link"
+                className={clsx(
+                  "flex p-0 h-auto flex-col items-center gap-0 cursor-default",
+                  {
+                    "pb-1": !item.isActive,
+                  }
+                )}
+              >
+                <img src={item.iconPath} width={55} />
+                {item.isActive && <ActiveIcon fill="#808080" />}
+              </Button>
+            );
+          } else if (item.type === DockItemType.SEPARATOR) {
+            return <div className="w-[1px] bg-[#E8E8E8] h-11 mb-1 mx-2"></div>;
+          }
         })}
       </div>
     </div>
